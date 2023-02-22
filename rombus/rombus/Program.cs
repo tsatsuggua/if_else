d90b6b047ec677655621a0f60d4b9ed7dace988d
Ivan Kazakov <124075660+tsatsuggua@users.noreply.github.com>
2023-02-22 16:28:11 +0200

    Rombus

---
+++ rombus/rombus/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace rombus
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Высота ромба (не менее 3 строк): ");
+            var h = int.Parse(Console.ReadLine());
+            h = h % 2 == 0 ? h + 1 : h;
+
+            for (var i = 0; i < h; i++)
+            {
+                var spacesBefore = Math.Abs(h / 2 - i);
+                var starsCount = (h - 2 * spacesBefore);
+                Console.WriteLine("{0}{1}", new string(' ', spacesBefore), new string('*', starsCount));
+
+            }
+        }
+    }
+}
