6e8728f593517da11886882704661caf81cc3fa6
Ivan Kazakov <124075660+tsatsuggua@users.noreply.github.com>
2023-02-27 17:37:47 +0200

    home7

---
+++ HomeWork7/HomeWork7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+
+namespace HomeWork7
+{
+    internal class Program
+    {
+
+        static void Recursion(int counter)
+        {
+            counter--;
+            Console.WriteLine($"First part: {counter}");
+
+            if (counter != 0)
+                SumTo(counter); 
+            Console.WriteLine($"Second part: {counter}");
+
+        }
+        static void SumTo(int counter)
+        {
+            Console.WriteLine($"First part: {counter}");
+
+            Recursion(counter);
+
+            Console.WriteLine($"Second part: {counter}");
+        
+        
+        }
+
+        static void Main()
+        {
+            SumTo(counter: 5);  
+            SumTo(counter: 1);  
+            SumTo(counter: 9);  
+            SumTo(counter: -8); 
+
+            Console.ReadKey();  
+        }
+    }
+}
